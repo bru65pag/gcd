de4ab6947b0ccb5527e51bb1012717b6c1674d9c
Bruno Pagis <bru65pag@gmail.com>
2019-10-03 12:13:17 +0200

    numbers input from command line

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
-fn main() {
-    println!("gcd(128,64) = {}",gcd(128,64));
-    println!("gcd(64,128) = {}",gcd(64,128));
-}
+use std::io::Write;
+use std::str::FromStr;
 
+fn main() {
+    let mut numbers = Vec::new();
+
+    for arg in std::env::args().skip(1) {
+        numbers.push(u64::from_str(&arg).expect("error parsing argument"));
+    }
+
+    if numbers.len() == 0 {
+        writeln!(std::io::stderr(), "Usage: gcd NUMBER ...").unwrap();
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    println!("The greatest common divisor of {:?} is {}",
+             numbers, d);
+}
 fn gcd(mut a:u64, mut b:u64) -> u64 {
     assert!(a!=0 && b!=0);
     while b!=0 {
